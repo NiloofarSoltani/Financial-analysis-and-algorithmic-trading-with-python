07440b4420248a623dfbb99d4a69b21dbcd2c485
RayanService <niloofarsoltani.3th@gmail.com>
2023-05-26 18:16:23 +0330

    add img for github add numpy exersice

--- basic_python_exercise.py
+++ basic_python_exercise.py
@@ -1,3 +1,4 @@
+
 import math
 
 # EXERCISE 1
---
+++ numpy_exercise.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+print('A list of even numbers with numpy:',np.arange(0,11,2))
+print('A list of odd numbers with numpy:',np.arange(1,11,2))
